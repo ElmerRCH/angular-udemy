28c9de613cda275fbda7455ed8350a87f4f53c4a
Elmer Camacho <renehiguera15@gmail.com>
2024-06-17 15:25:45 -0700

    logos

--- heroes/src/app/components/heroe/heroe.component.ts
+++ heroes/src/app/components/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
+import { RouterOutlet,RouterLink,RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -10,16 +11,19 @@ import { HeroesService } from '../../services/heroes.service';
   styleUrl: './heroe.component.css'
 })
 
+
 export class HeroeComponent {
   id :number = 0
   heroe:any = {};
-
+  img = ''
   constructor(private activatedRoute:ActivatedRoute,
               private heroesServices:HeroesService
   ){
     this.activatedRoute.params.subscribe( params => {
       this.id = parseInt(params['id'])
-      console.log('llego:::', this.id)
+      this.heroe = heroesServices.getHeroe(this.id)
+      this.img = this.heroe.casa == "Marvel" ? "../../../assets/img/Marvel_Logo.svg" : "../../../assets/img/logo_dc.png"
+
     });
       
   }
